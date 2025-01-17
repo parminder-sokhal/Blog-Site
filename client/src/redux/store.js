a9ca33619eeeba20303ea6bac04a2caedaa4836a
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-01-17 17:24:02 +0530

    changes

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore,combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+const rootReducer =combineReducers({
+  user: userReducer,
+})
 
 export const store = configureStore({
   reducer: { user: userReducer },
