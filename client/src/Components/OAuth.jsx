6790803500c1f1fed8792a7896a85b191a7f2823
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-01-20 17:11:07 +0530

    changes

---
+++ client/src/Components/OAuth.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'flowbite-react'
+import { AiFillGoogleCircle } from "react-icons/ai";
+
+function OAuth() {
+  return (
+    <Button type='butto' gradientDuoTone='pinkToOrange' outline>
+      <AiFillGoogleCircle/>
+    </Button>
+  )
+}
+
+export default OAuth;
