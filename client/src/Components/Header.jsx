e9817f5dae9945d9256690601ba5a39401e76ee6
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-01-23 12:27:47 +0530

    profileImage

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,52 +1,76 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
-import { CiSearch } from 'react-icons/ci';
-import { FaMoon } from 'react-icons/fa';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector((state) => state.user);
   return (
-    <Navbar className='border-b-2'>
+    <Navbar className="border-b-2">
       <Link
-        to='/'
-        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 ring-offset-pink-500 rounded-lg text-white'>
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 ring-offset-pink-500 rounded-lg text-white">
           Parminder
         </span>
         Sokhal
       </Link>
       <form>
         <TextInput
-          type='text'
-          placeholder='search...'
+          type="text"
+          placeholder="search..."
           rightIcon={CiSearch}
-          className='hidden lg:inline'
+          className="hidden lg:inline"
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+      <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <CiSearch />
       </Button>
-      <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+      <div className="flex gap-2 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to='/signin'>
-          <Button gradientDuoTone='purpleToBlue' outline>
-            Sign In
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+<Dropdown.Header>
+
+  <span className="block text-sm ">@{currentUser.username}</span>
+  <span className="block text-sm font-medium truncate">@{currentUser.email}</span>
+</Dropdown.Header>
+<Link to={'/dashboard?tab=profile'}>
+<Dropdown.Item>profile</Dropdown.Item>
+</Link>
+<Dropdown.Divider/>
+<Dropdown.Item>Sign Out</Dropdown.Item>
+
+          </Dropdown>
+        ) : (
+          <Link to="/signin">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/Projects'>Projects</Link>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
+          <Link to="/Projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
